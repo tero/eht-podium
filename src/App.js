a56fdb209343e2c4047d7da5d31a98d16be544ba
Tero Alén <tero@alen.mobi>
2017-04-22 21:32:49 +0300

    dynamic name list

--- src/App.js
+++ src/App.js
@@ -2,32 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        gold: [
+            'Simo',
+        ],
+        silver: [
+            'Simo',
+            'Helmi',
+        ],
+        bronze: [
+        ]
+    }
+  }
   render() {
+    const { gold, silver, bronze } = this.state
+
     return (
       <div className="App">
         <div className="podium-container">
             <div className="column">
-                <div>Simo</div>
-                <div>Helmi</div>
-                <div>Helmi</div>
-                <div>Helmi</div>
-                <div>Helmi</div>
-                <div>Helmi</div>
-                <div>Helmi</div>
-                <div>Helmi</div>
+                {silver.map(name => <div key={name}>{name}</div>)}
                 <div className="podium silver">
                     2
                 </div>
             </div>
             <div className="column">
-                <div>Tero</div>
-                <div>Juho</div>
+                {gold.map(name => <div key={name}>{name}</div>)}
                 <div className="podium gold">
                     1
                 </div>
             </div>
             <div className="column">
-                <div>Sissel</div>
+                {bronze.map(name => <div key={name}>{name}</div>)}
                 <div className="podium bronze">
                     3
                 </div>
